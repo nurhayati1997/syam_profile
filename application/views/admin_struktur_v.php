292e5e24ea5a7a524d9e6a2e16338d758f56bc5f
nurhayati1997 <nurhayatifitriani77@gmail.com>
2021-12-24 09:58:54 +0700

    admin struktur

---
+++ application/views/admin_struktur_v.php
@@ -0,0 +1,129 @@
+<!-- PAGE CONTAINER -->
+    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <div id="root" class="root mn--max hd--expanded">
+
+       <!-- CONTENTS -->
+        <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+        <section id="content" class="content">
+            <div class="content__header content__boxed overlapping">
+                <div class="content__wrap pt-3 pb-4">
+                    <h1 class="page-title mb-2 mt-4">Tentang Kami - Struktur</h1>
+
+                    <p class="lead">
+                       RSUD SYAMRABU BANGKALAN
+                    </p>
+
+                </div>
+
+            </div>
+
+            <div class="content__boxed">
+                <div class="content__wrap">
+
+                    <!-- Table with toolbar -->
+                    <div class="card">
+                        <div class="card-header -4 mb-4">
+                            <!-- <h5 class="card-title mb-4">Table with toolbar</h5> -->
+                            <div class="row">
+
+                               
+
+                                <!-- Right Toolbar -->
+                                <div class="col-md-12 d-flex gap-1 align-items-center justify-content-md-end mb-4">
+                                    <!-- FORM -->
+                                    <div class="card">
+                                        <div class="card-header toolbar">
+                                            <div class="toolbar-start">
+                                                <h5 class="m-0">Tambah SDM</h5>
+                                            </div>
+                                            <div class="toolbar-end">
+                                                <button type="button" class="btn btn-icon btn-light btn-xs" data-bs-toggle="collapse" data-bs-target="#_dm-collapsedByDefault" aria-expanded="true" aria-controls="_dm-collapsedByDefault">
+                                                    <i class="demo-psi-min"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div id="_dm-collapsedByDefault" class="collapse">
+                                            <div class="card-body">
+                                                <form class="row g-3">
+
+                                                    <div class="col-md-4">
+                                                        <label for="_dm-inputNama_k" class="form-label">Total Nakes</label>
+                                                        <input id="_dm-inputNama_k" type="text" class="form-control">
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <label for="_dm-inputNama_k" class="form-label">Total Struktural</label>
+                                                        <input id="_dm-inputNama_k" type="text" class="form-control">
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <label for="_dm-inputNama_k" class="form-label">Total Non Nakes</label>
+                                                        <input id="_dm-inputNama_k" type="text" class="form-control">
+                                                    </div>
+
+                                                    
+                                                    
+                                                    <div class="col-12">
+                                                        <button type="submit" class="btn btn-primary">Tambah</button>
+                                                    </div>
+                                                </form>
+                                                <!-- <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.</p> -->
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- END : Right Toolbar -->
+
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="table-responsove">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Total Nakes</th>
+                                            <th>Total Struktural</th>
+                                            <th>Total Nakes</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>566 +</td>
+                                            <td>27 +</td>
+                                            <td>469 +</td>
+                                        </tr>
+                                        
+                                        
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- END : Table with toolbar -->
+
+                </div>
+            </div>
+            <!-- FOOTER -->
+            <footer class="border-top mt-auto">
+                <div class="content__boxed">
+                    <div class="content__wrap py-3 py-md-1 d-flex flex-column flex-md-row align-items-md-center">
+                        <div class="text-nowrap mb-4 mb-md-0">Copyright &copy; 2021 <a href="#" class="h6 text-decoration-none ms-1">My Company</a></div>
+                        <nav class="nav flex-column gap-1 flex-md-row gap-md-3 ms-md-auto me-5" style="row-gap: 0 !important;">
+                            <a class="nav-link px-0" href="#">Policy Privacy</a>
+                            <a class="nav-link px-0" href="#">Terms and conditions</a>
+                            <a class="nav-link px-0" href="#">Contact Us</a>
+                        </nav>
+                    </div>
+                </div>
+            </footer>
+            <!-- END - FOOTER -->
+
+        </section>
+
+        <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+        <!-- END - CONTENTS -->
+
+    </div>
+    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <!-- END - PAGE CONTAINER -->
