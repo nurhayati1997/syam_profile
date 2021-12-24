2940559f7787838fd33731b9dab500c4e0e47c3b
nurhayati1997 <nurhayatifitriani77@gmail.com>
2021-12-24 09:23:08 +0700

    admin pendidikan

--- application/views/login_v.php
+++ application/views/login_v.php
@@ -7,7 +7,7 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
     <meta name="description" content="LockScreen is designed to allow the user to lock sensitive pages with a password protected.">
-    <title>E-DOKAR | SYAMRABU</title>
+    <title>Web Profile | SYAMRABU</title>
 
     <!-- STYLESHEETS -->
     <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~--- -->
