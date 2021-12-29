ace84410ef89c85040dd55ecf106aac69b420881
unknown <nurhayatifitriani77@gmail.com>
2021-12-29 10:34:03 +0700

    jejaring rs

--- application/views/Pendidikan_v.php
+++ application/views/Pendidikan_v.php
@@ -277,95 +277,16 @@ img.emoji {
 <div class="elementor-widget-wrap">
 <div class="elementor-element elementor-element-cf2f730 elementor-widget elementor-widget-image" data-id="cf2f730" data-element_type="widget" data-widget_type="image.default">
 <div class="elementor-widget-container">
-<div class="elementor-image">
-<a href="../index.html">
-<img src="wp-content/uploads/elementor/thumbs/logo-2-og82lchzwe2h4fi9x469v5aiuvok36tnwqkz74djh4.png" title="logo-2" alt="logo-2" /> </a>
-</div>
-</div>
-</div>
-<div class="elementor-element elementor-element-4568037 elementor-widget elementor-widget-heading" data-id="4568037" data-element_type="widget" data-widget_type="heading.default">
-<div class="elementor-widget-container">
-<h2 class="elementor-heading-title elementor-size-default">About Us</h2> </div>
-</div>
-<div class="elementor-element elementor-element-e2236e0 elementor-widget elementor-widget-text-editor" data-id="e2236e0" data-element_type="widget" data-widget_type="text-editor.default">
-<div class="elementor-widget-container">
-<div class="elementor-text-editor elementor-clearfix">
-<p>We must explain to you how all seds this mistakens idea off denouncing pleasures and praising pain was born and I will give you a completed accounts off the system and expound.</p> </div>
-</div>
-</div>
+
 <div class="elementor-element elementor-element-774daf5 elementor-widget elementor-widget-elementskit-button" data-id="774daf5" data-element_type="widget" data-widget_type="elementskit-button.default">
 <div class="elementor-widget-container">
 <div class="ekit-wid-con"> <div class="ekit-btn-wraper">
-<a href="#" class="elementskit-btn  whitespace--normal">
-Consultation </a>
+<a href="<?php echo site_url('login');?>" class="elementskit-btn  whitespace--normal">
+Admin </a>
 </div>
 </div> </div>
 </div>
-<div class="elementor-element elementor-element-adcb817 elementor-widget elementor-widget-heading" data-id="adcb817" data-element_type="widget" data-widget_type="heading.default">
-<div class="elementor-widget-container">
-<h2 class="elementor-heading-title elementor-size-default">Contact Info</h2> </div>
-</div>
-<div class="elementor-element elementor-element-be2e17b elementor-icon-list--layout-traditional elementor-list-item-link-full_width elementor-widget elementor-widget-icon-list" data-id="be2e17b" data-element_type="widget" data-widget_type="icon-list.default">
-<div class="elementor-widget-container">
-<ul class="elementor-icon-list-items">
-<li class="elementor-icon-list-item">
-<span class="elementor-icon-list-icon">
-<i aria-hidden="true" class="fas fa-map-marked-alt"></i> </span>
-<span class="elementor-icon-list-text">Rock St 12, Newyork City, USA</span>
-</li>
-<li class="elementor-icon-list-item">
-<a href="tel:0000000000000">
-<span class="elementor-icon-list-icon">
-<i aria-hidden="true" class="fas fa-phone-alt"></i> </span>
-<span class="elementor-icon-list-text">(000) 000-000-0000</span>
-</a>
-</li>
-<li class="elementor-icon-list-item">
-<a href="https://wp.xpeedstudio.com/cdn-cgi/l/email-protection#7e131b1a17041d113e19131f1712501d1113">
-<span class="elementor-icon-list-icon">
-<i aria-hidden="true" class="far fa-envelope"></i> </span>
-<span class="elementor-icon-list-text"><span class="__cf_email__" data-cfemail="9ed3fbfaf7e4fdf1def9f3fff7f2b0fdf1f3">[email&#160;protected]</span></span>
-</a>
-</li>
-<li class="elementor-icon-list-item">
-<span class="elementor-icon-list-icon">
-<i aria-hidden="true" class="far fa-clock"></i> </span>
-<span class="elementor-icon-list-text">Week Days: 09.00 to 18.00 Sunday: Closed</span>
-</li>
-</ul>
-</div>
-</div>
-<div class="elementor-element elementor-element-d834e99 elementor-widget elementor-widget-elementskit-social-media" data-id="d834e99" data-element_type="widget" data-widget_type="elementskit-social-media.default">
-<div class="elementor-widget-container">
-<div class="ekit-wid-con"> <ul class="ekit_social_media">
-<li class="elementor-repeater-item-bb0a9dd">
-<a href="https://facebook.com/" target="_blank" class="facebook">
-<i aria-hidden="true" class="fasicon icon-facebook"></i>
-</a>
-</li>
-<li class="elementor-repeater-item-525770f">
-<a href="https://facebook.com/" target="_blank" class="twitter">
-<i aria-hidden="true" class="fasicon icon-twitter"></i>
-</a>
-</li>
-<li class="elementor-repeater-item-3b5b739">
-<a href="https://facebook.com/" target="_blank" class="linkedin">
-<i aria-hidden="true" class="fasicon icon-linkedin"></i>
-</a>
-</li>
-<li class="elementor-repeater-item-f3e4f69">
-<a href="https://facebook.com/" target="_blank" class="instagram1">
-<i aria-hidden="true" class="fasicon icon-instagram1"></i>
-</a>
-</li>
-<li class="elementor-repeater-item-0d29ad9">
-<a href="https://facebook.com/" target="_blank" class="youtube">
-<i aria-hidden="true" class="fasicon icon-youtube"></i>
-</a>
-</li>
-</ul>
-</div> </div>
-</div>
+
 </div>
 </div>
 </div>
@@ -1022,9 +943,9 @@ Jumlah Jejaring
 </div>
 <div class="funfact-content">
 <div class="number-percentage-wraper">
-<span class="number-percentage" data-value="956" data-animation-duration="3500">0</span>
+<span class="number-percentage" data-value="3" data-animation-duration="3500">0</span>
 + </div>
-<p class="funfact-title">Skincare Treatments</p> </div>
+<p class="funfact-title">STIKES</p> </div>
 </div>
 </div>
 </div> </div>
@@ -1045,9 +966,9 @@ Jumlah Jejaring
 </div>
 <div class="funfact-content">
 <div class="number-percentage-wraper">
-<span class="number-percentage" data-value="956" data-animation-duration="3500">0</span>
+<span class="number-percentage" data-value="7" data-animation-duration="3500">0</span>
 + </div>
-<p class="funfact-title">Skincare Treatments</p> </div>
+<p class="funfact-title">RS</p> </div>
 </div>
 </div>
 </div> </div>
@@ -1055,6 +976,7 @@ Jumlah Jejaring
 </div>
 </div>
 </div>
+
 <div class="elementor-column elementor-col-20 elementor-inner-column elementor-element elementor-element-eab34c0 elementor-invisible" data-id="eab34c0" data-element_type="column" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;animation_delay&quot;:600}">
 <div class="elementor-column-wrap elementor-element-populated">
 <div class="elementor-widget-wrap">
@@ -1068,9 +990,9 @@ Jumlah Jejaring
 </div>
 <div class="funfact-content">
 <div class="number-percentage-wraper">
-<span class="number-percentage" data-value="956" data-animation-duration="3500">0</span>
+<span class="number-percentage" data-value="5" data-animation-duration="3500">0</span>
 + </div>
-<p class="funfact-title">Skincare Treatments</p> </div>
+<p class="funfact-title">Universitas</p> </div>
 </div>
 </div>
 </div> </div>
@@ -1078,6 +1000,81 @@ Jumlah Jejaring
  </div>
 </div>
 </div>
+
+
+<div class="elementor-column elementor-col-20 elementor-inner-column elementor-element elementor-element-c5dfe16 elementor-invisible" data-id="c5dfe16" data-element_type="column" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;animation_delay&quot;:400}">
+<div class="elementor-column-wrap elementor-element-populated">
+<div class="elementor-widget-wrap">
+<div class="elementor-element elementor-element-2c63475 elementor-widget elementor-widget-elementskit-funfact" data-id="2c63475" data-element_type="widget" data-widget_type="elementskit-funfact.default">
+<div class="elementor-widget-container">
+<div class="ekit-wid-con">
+<div class="elementskit-funfact text-center     ">
+<div class="elementskit-funfact-inner">
+<div class="funfact-icon"> <i></i>
+<i class="icon icon-abacus1 elementskit-funfact-icon" aria-hidden="true"></i>
+</div>
+<div class="funfact-content">
+<div class="number-percentage-wraper">
+<span class="number-percentage" data-value="2" data-animation-duration="3500">0</span>
++ </div>
+<p class="funfact-title">Poltekes</p> </div>
+</div>
+</div>
+</div> </div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="elementor-column elementor-col-20 elementor-inner-column elementor-element elementor-element-ed9710d elementor-invisible" data-id="ed9710d" data-element_type="column" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;animation_delay&quot;:200}">
+<div class="elementor-column-wrap elementor-element-populated">
+<div class="elementor-widget-wrap">
+<div class="elementor-element elementor-element-116fbe5 elementor-widget elementor-widget-elementskit-funfact" data-id="116fbe5" data-element_type="widget" data-widget_type="elementskit-funfact.default">
+<div class="elementor-widget-container">
+<div class="ekit-wid-con">
+<div class="elementskit-funfact text-center     ">
+<div class="elementskit-funfact-inner">
+<div class="funfact-icon"> <i></i>
+<i class="icon icon-atomic elementskit-funfact-icon" aria-hidden="true"></i>
+</div>
+<div class="funfact-content">
+<div class="number-percentage-wraper">
+<span class="number-percentage" data-value="2" data-animation-duration="3500">0</span>
++ </div>
+<p class="funfact-title">PMI</p> </div>
+</div>
+</div>
+</div> </div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="elementor-column elementor-col-20 elementor-inner-column elementor-element elementor-element-eab34c0 elementor-invisible" data-id="eab34c0" data-element_type="column" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;,&quot;animation_delay&quot;:600}">
+<div class="elementor-column-wrap elementor-element-populated">
+<div class="elementor-widget-wrap">
+<div class="elementor-element elementor-element-2db0854 elementor-widget elementor-widget-elementskit-funfact" data-id="2db0854" data-element_type="widget" data-widget_type="elementskit-funfact.default">
+<div class="elementor-widget-container">
+<div class="ekit-wid-con">
+<div class="elementskit-funfact text-center     ">
+<div class="elementskit-funfact-inner">
+<div class="funfact-icon"> <i></i>
+<i class="icon icon-trophy1 elementskit-funfact-icon" aria-hidden="true"></i>
+</div>
+<div class="funfact-content">
+<div class="number-percentage-wraper">
+<span class="number-percentage" data-value="1" data-animation-duration="3500">0</span>
++ </div>
+<p class="funfact-title">LAB</p> </div>
+</div>
+</div>
+</div> </div>
+</div>
+ </div>
+</div>
+</div>
+
 <div class="elementor-column elementor-col-20 elementor-inner-column elementor-element elementor-element-d426f9b elementor-hidden-desktop elementor-hidden-tablet elementor-hidden-phone" data-id="d426f9b" data-element_type="column">
 <div class="elementor-column-wrap">
 <div class="elementor-widget-wrap">
