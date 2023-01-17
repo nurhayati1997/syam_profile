74db7f6286940338bb9e9fed4e3d32304da51191
unknown <nurhayatifitriani77@gmail.com>
2023-01-17 11:27:56 +0700

    kuis

---
+++ application/views/Kuis_v.php
@@ -0,0 +1,103 @@
+<div id="post-2" class="home-page-contents post-2 page type-page status-publish hentry" role="main">
+	<div class="builder-content">
+		<div data-elementor-type="wp-post" data-elementor-id="2" class="elementor elementor-2" data-elementor-settings="[]">
+			<div class="elementor-inner">
+				<div class="elementor-section-wrap">
+					<section class="elementor-section elementor-top-section elementor-element elementor-element-6b4cda1 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="6b4cda1" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+						<div class="elementor-container elementor-column-gap-default">
+							<div class="elementor-row">
+								<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-f725996" data-id="f725996" data-element_type="column">
+									<div class="elementor-column-wrap elementor-element-populated">
+										<div class="elementor-widget-wrap">
+											<div class="elementor-element elementor-element-03af56e ekit-equal-height-disable elementor-invisible elementor-widget elementor-widget-elementskit-icon-box" data-id="03af56e" data-element_type="widget" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:0}" data-widget_type="elementskit-icon-box.default">
+												<div class="elementor-widget-container">
+													<div class="ekit-wid-con">
+
+														<div class="elementskit-infobox text- text-left icon-lef-right-aligin elementor-animation- media  ">
+															<div class="elementskit-box-header elementor-animation-">
+																<div class="elementskit-info-box-icon  text-center">
+																	<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="Layer_1" x="0px" y="0px" viewBox="0 0 217.23 250" xml:space="preserve">
+																		<g>
+																			<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="14.1724" y1="146.7394" x2="193.6281" y2="146.7394">
+																				<stop offset="0" style="stop-color:#1CBA9F"></stop>
+																				<stop offset="0.8" style="stop-color:#FFFFFF;stop-opacity:0"></stop>
+																			</linearGradient>
+																			<circle opacity="0.1" fill="url(#SVGID_1_)" cx="103.9" cy="146.74" r="89.73"></circle>
+																			<g>
+																				<path fill="#2E3192" d="M189.94,141.52c-0.62-11.24-3.26-21.39-7.88-31.04c-15.58-32.52-47.93-51.91-83.98-50.33    c-40.62,1.78-75.84,33.76-81.51,74.03c-6.69,47.53,23.47,90.35,70.68,98.98c28.97,5.3,55.33-2.57,79.22-19.34    c0.68-0.48,1.3-1.27,2.04-1.4c1.06-0.19,2.71-0.31,3.2,0.28c0.63,0.77,0.54,2.29,0.43,3.45c-0.06,0.63-0.82,1.2-1.3,1.76    c-20.16,23.98-46.09,34.5-77.05,31.6c-36.04-3.37-63.46-21.23-80.95-52.8C-20.87,135.91,15,59.91,83.23,46.52    c57.68-11.32,113.65,28.82,121.22,86.94c0.1,0.74,0.25,1.47,0.34,2.2c0.71,5.86,0.71,5.86,6.51,5.86c0.74,0,1.49-0.01,2.23,0    c3.59,0.07,4.64,1.89,2.83,5.04c-5.41,9.41-10.84,18.81-16.3,28.19c-1.84,3.16-3.95,3.18-5.77,0.05    c-5.46-9.38-10.89-18.78-16.29-28.2c-1.88-3.28-0.8-5.05,3.03-5.08C183.88,141.51,186.74,141.52,189.94,141.52z"></path>
+																				<path fill="#2E3192" d="M57.41,189.79c-0.6-4.61-0.14-8.54,2.2-12.52c9.11-15.52,17.91-31.22,26.73-46.9    c0.77-1.37,1.17-3.11,1.23-4.7c0.16-4.62,0.06-9.26,0.06-14.1c-5.56,0-10.89,0-16.57,0c0,5.34,0,10.69,0,16.26    c-4.45,0-8.55,0-12.79,0c0-7.32-0.23-14.62,0.1-21.9c0.16-3.51,2.63-5.52,6.21-5.57c9.84-0.14,19.68-0.15,29.51,0    c3.96,0.06,6.35,2.49,6.48,6.6c0.21,6.68,0.38,13.39-0.11,20.03c-0.22,3-1.7,6.07-3.21,8.78c-7.43,13.27-15.06,26.43-22.62,39.64    c-0.49,0.86-0.93,1.74-1.6,3c9.35,0,18.22,0,27.32,0c0,3.9,0,7.55,0,11.38C86.06,189.79,71.82,189.79,57.41,189.79z"></path>
+																				<path fill="#2E3192" d="M122.04,161.67c5.15,0,9.76,0,14.7,0c0-8.9,0-17.67,0-26.67c4.27,0,8.2,0,12.56,0c0,8.82,0,17.66,0,26.75    c2.43,0,4.44,0,6.72,0c0,3.79,0,7.33,0,11.22c-2.11,0-4.19,0-6.62,0c0,5.76,0,11.18,0,16.79c-4.27,0-8.2,0-12.51,0    c0-5.5,0-10.93,0-16.63c-9.98,0-19.59,0-29.89,0c0.27-3.38-0.09-6.66,0.86-9.49c6.84-20.48,13.97-40.87,20.95-61.31    c0.53-1.54,1.22-2.21,2.93-2.15c3.68,0.13,7.37,0.04,11.4,0.04C136.07,120.83,129.12,141.04,122.04,161.67z"></path>
+																				<path fill="#2E3192" d="M103.13,0.01c8.08,0,16.15-0.02,24.23,0.01c3.29,0.01,4.11,0.83,4.13,4.13c0.03,6.68,0.03,13.37,0,20.05    c-0.02,3.01-0.99,3.95-4.01,4.03c-1.58,0.04-3.16-0.01-4.73,0.02c-3.22,0.06-4.23,1.1-4.28,4.42c-0.01,0.56,0,1.11,0,1.67    c-0.06,3.29-1.03,3.96-4.24,3.83c-7.22-0.28-14.45-0.4-21.68-0.47c-3.9-0.03-4.49-0.5-4.56-4.45c-0.07-4.11-0.98-4.99-5.15-5.01    c-8.04-0.03-8.04-0.03-8.04-8.16c0-5.2-0.01-10.4,0-15.59c0.01-3.58,0.87-4.46,4.4-4.47C87.17-0.01,95.15,0,103.13,0.01z"></path>
+																				<path fill="#2E3192" d="M192.38,49.18c-0.39,0.87-0.56,1.64-1.01,2.17c-1.96,2.35-3.94,4.67-6.02,6.92    c-1.56,1.68-3.21,1.39-4.91,0.03c-3.81-3.06-4.43-3.03-7.72,0.62c-1.54,1.7-2.96,2.82-5.1,0.89c-1.5-1.35-1.93-2.64-0.3-4.18    c0.6-0.57,1.12-1.23,1.65-1.88c1.9-2.31,1.83-3.73-0.28-5.74c-3.35-3.19-3.39-3.85-0.41-7.25c1.46-1.67,2.92-3.35,4.39-5.02    c1.57-1.78,3.31-1.92,5.1-0.37c4.34,3.76,8.68,7.53,12.95,11.36C191.43,47.36,191.82,48.33,192.38,49.18z"></path>
+																				<path fill="#2E3192" d="M105.03,71.21c0,1.29,0.13,2.6-0.06,3.85c-0.08,0.52-0.88,1.28-1.35,1.28c-0.47,0-1.32-0.81-1.34-1.29    c-0.11-2.48-0.08-4.97,0.09-7.44c0.03-0.49,0.86-0.92,1.33-1.38c0.46,0.46,1.23,0.88,1.32,1.41c0.2,1.16,0.06,2.38,0.06,3.58    C105.06,71.21,105.04,71.21,105.03,71.21z"></path>
+																				<path fill="#2E3192" d="M69.27,84.53c-0.41,0.54-0.66,1.19-1.04,1.29c-0.5,0.13-1.41,0-1.63-0.34c-1.42-2.26-2.75-4.59-3.98-6.97    c-0.18-0.36,0.2-1.22,0.58-1.58c0.26-0.25,1.27-0.25,1.42-0.01C66.21,79.39,67.7,81.92,69.27,84.53z"></path>
+																				<path fill="#2E3192" d="M42.91,182.12c-0.84,1.08-1.07,1.62-1.49,1.87c-2.05,1.25-4.1,2.5-6.25,3.56    c-0.53,0.26-1.41-0.2-2.13-0.34c0.25-0.69,0.29-1.7,0.78-2.02c2-1.32,4.09-2.52,6.25-3.55C40.66,181.37,41.61,181.88,42.91,182.12    z"></path>
+																				<path fill="#2E3192" d="M34.76,105.38c2.61,1.62,5.14,3.14,7.61,4.77c0.23,0.15,0.21,1.17-0.04,1.43    c-0.36,0.37-1.22,0.72-1.58,0.53c-2.37-1.25-4.69-2.6-6.95-4.05c-0.34-0.22-0.44-1.15-0.3-1.66    C33.61,106.01,34.26,105.76,34.76,105.38z"></path>
+																				<path fill="#2E3192" d="M173.69,106.83c-0.02,1.91-7.77,6.4-8.61,5.55c-0.4-0.4-0.34-2.04,0.07-2.32c2.04-1.4,4.21-2.64,6.44-3.7    C172.14,106.1,173.09,106.68,173.69,106.83z"></path>
+																				<path fill="#2E3192" d="M171.67,188.51c-2.53-1.51-4.98-2.94-7.37-4.47c-0.28-0.18-0.41-1.13-0.2-1.43    c0.31-0.43,1.19-0.95,1.49-0.79c2.5,1.34,4.95,2.8,7.34,4.32c0.26,0.16,0.27,1.08,0.07,1.48    C172.82,188.01,172.2,188.19,171.67,188.51z"></path>
+																				<path fill="#2E3192" d="M62.84,217.39c-0.32-0.75-1.07-1.67-0.85-2.11c1.17-2.39,2.53-4.68,3.96-6.93    c0.23-0.36,1.15-0.49,1.66-0.37c0.35,0.09,0.87,0.99,0.75,1.22c-1.38,2.58-2.87,5.11-4.36,7.63    C63.89,217.02,63.53,217.07,62.84,217.39z"></path>
+																				<path fill="#2E3192" d="M143.38,217.31c-1.94,0.03-6.46-7.63-5.63-8.54c0.37-0.4,2.03-0.38,2.3,0c1.4,2.04,2.64,4.21,3.72,6.44    C144.03,215.77,143.5,216.71,143.38,217.31z"></path>
+																				<path fill="#2E3192" d="M144.95,78.41c-1.58,2.6-3.07,5.12-4.67,7.57c-0.16,0.25-1.28,0.26-1.44,0.03    c-0.35-0.51-0.7-1.42-0.47-1.87c1.13-2.21,2.38-4.36,3.74-6.43c0.28-0.43,1.21-0.61,1.83-0.58    C144.27,77.16,144.56,77.89,144.95,78.41z"></path>
+																				<path fill="#2E3192" d="M104.21,222.58c0,1.2,0.15,2.44-0.06,3.6c-0.11,0.61-0.88,1.09-1.35,1.63c-0.44-0.49-1.23-0.95-1.25-1.46    c-0.12-2.49-0.12-5,0.03-7.5c0.03-0.48,0.88-0.92,1.36-1.37c0.43,0.49,1.15,0.94,1.24,1.49c0.19,1.17,0.06,2.4,0.06,3.6    C104.23,222.58,104.22,222.58,104.21,222.58z"></path>
+																				<path fill="#2E3192" d="M27.77,145.43c1.2,0,2.43-0.14,3.6,0.06c0.52,0.09,0.93,0.86,1.39,1.32c-0.48,0.44-0.95,1.23-1.46,1.25    c-2.49,0.13-5,0.12-7.49-0.02c-0.48-0.03-0.92-0.86-1.38-1.33c0.49-0.44,0.93-1.18,1.47-1.25c1.27-0.19,2.58-0.06,3.88-0.06    C27.77,145.41,27.77,145.42,27.77,145.43z"></path>
+																			</g>
+																		</g>
+																	</svg>
+																</div>
+															</div>
+															<div class="box-body">
+																<h3 class="elementskit-info-box-title">
+																	WELCOME </h3>
+																<p>KUIS PENINGKATAN PENGETAHUAN KARYAWAN </p>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="elementor-element elementor-element-8b55d0f elementor-invisible elementor-widget elementor-widget-text-editor" data-id="8b55d0f" data-element_type="widget" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:300}" data-widget_type="text-editor.default">
+												<div class="elementor-widget-container">
+													<div class="elementor-text-editor elementor-clearfix">
+														<p>Sebelum mengerjakan KUIS pastikan anda dalam keadaan TENANG
+															& sudah mempelajari informasi pelayanan UOBK RSUD SYAMRABU Bangkalan.
+															Maksimal waktu pengerjaan adalah 120 Menit. <br>SELAMAT MENGERJAKAN
+														</p>
+													</div>
+												</div>
+											</div>
+											<div class="elementor-element elementor-element-da17ee5 tel_num ekit-equal-height-disable elementor-invisible elementor-widget elementor-widget-elementskit-icon-box" data-id="da17ee5" data-element_type="widget" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:600}" data-widget_type="elementskit-icon-box.default">
+												<div class="elementor-widget-container">
+													<div class="ekit-wid-con">
+														<a href="<?php echo site_url('Login'); ?>" class="ekit_global_links">
+															<div class="elementskit-infobox text- text-left icon-lef-right-aligin elementor-animation- media  ">
+																<div class="elementskit-box-header elementor-animation-">
+																	<div class="elementskit-info-box-icon  text-center">
+																		<i class="icon icon-phone elementkit-infobox-icon" aria-hidden="true"></i>
+																	</div>
+																</div>
+																<div class="box-body">
+																	<h3 class="elementskit-info-box-title">
+																		START KUIS </h3>
+																</div>
+															</div>
+														</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-2dc7936" data-id="2dc7936" data-element_type="column">
+									<div class="elementor-column-wrap">
+										<div class="elementor-widget-wrap">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- application/views/login_v.php
+++ application/views/login_v.php
@@ -1,149 +1,217 @@
 <!DOCTYPE html>
+<!--
+Template Name: Metronic - Bootstrap 4 HTML, React, Angular 11 & VueJS Admin Dashboard Theme
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: https://1.envato.market/EA4JP
+Renew Support: https://1.envato.market/EA4JP
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
 <html lang="en">
+<!--begin::Head-->
 
+<!-- Mirrored from preview.keenthemes.com/metronic/demo2/custom/pages/login/classic/login-4.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 13 Jan 2022 06:40:32 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 
-<!-- Mirrored from themeon.net/nifty/v3.0.0/front-pages/lock-screen/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 21 Dec 2021 05:26:45 GMT -->
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
-    <meta name="description" content="LockScreen is designed to allow the user to lock sensitive pages with a password protected.">
-    <title>Web Profile | SYAMRABU</title>
-
-    <!-- STYLESHEETS -->
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~--- -->
-
-    <!-- Fonts [ OPTIONAL ] -->
-    <link rel="preconnect" href="https://fonts.googleapis.com/">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&amp;family=Ubuntu:wght@400;500;700&amp;display=swap" rel="stylesheet">
-
-    <!-- Bootstrap CSS [ REQUIRED ] -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.24323102f2dae5917bf32bd9a470b04ef921f8c64a12388fb0c548fe5dbc97d2.css">
-
-    <!-- Nifty CSS [ REQUIRED ] -->
-    <link rel="stylesheet" href="assets/css/nifty.min.b62b57e9970804afb27aad6829d5d500079b5caeacab36cb742a093376542acd.css">
-
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---
-
-    [ REQUIRED ]
-    You must include this category in your project.
-
-
-    [ OPTIONAL ]
-    This is an optional plugin. You may choose to include it in your project.
-
-
-    [ DEMO ]
-    Used for demonstration purposes only. This category should NOT be included in your project.
-
-
-    [ SAMPLE ]
-    Here's a sample script that explains how to initialize plugins and/or components: This category should NOT be included in your project.
-
-
-    Detailed information and more samples can be found in the documentation.
-
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~--- -->
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&amp;l=' + l : '';
+			j.async = true;
+			j.src = '../../../../../../../www.googletagmanager.com/gtm5445.html?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-5FS8GGP');
+	</script>
+	<!-- End Google Tag Manager -->
+	<meta charset="utf-8" />
+	<title>KUIS to KARYAWAN</title>
+	<meta name="description" content="Login page example" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<link rel="canonical" href="https://keenthemes.com/metronic" />
+	<!--begin::Fonts-->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+	<!--end::Fonts-->
+	<!--begin::Page Custom Styles(used by this page)-->
+	<link href="assets_login/css/pages/login/classic/login-45883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+	<!--end::Page Custom Styles-->
+	<!--begin::Global Theme Styles(used by all pages)-->
+	<link href="assets_login/plugins/global/plugins.bundle5883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+	<link href="assets_login/plugins/custom/prismjs/prismjs.bundle5883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+	<link href="assets_login/css/style.bundle5883.css?v=7.2.9" rel="stylesheet" type="text/css" />
+	<!--end::Global Theme Styles-->
+	<!--begin::Layout Themes(used by all pages)-->
+	<!--end::Layout Themes-->
+	<link rel="shortcut icon" href="assets_login/logot.png" />
+	<!-- Hotjar Tracking Code for keenthemes.com -->
+	<script>
+		(function(h, o, t, j, a, r) {
+			h.hj = h.hj || function() {
+				(h.hj.q = h.hj.q || []).push(arguments)
+			};
+			h._hjSettings = {
+				hjid: 1070954,
+				hjsv: 6
+			};
+			a = o.getElementsByTagName('head')[0];
+			r = o.createElement('script');
+			r.async = 1;
+			r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+			a.appendChild(r);
+		})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+	</script>
 </head>
+<!--end::Head-->
+<!--begin::Body-->
 
-<body class="">
+<body id="kt_body" style="background-image: url(assets_login/media/bg/bg-11.jpg)" class="quick-panel-right demo-panel-right offcanvas-right header-fixed subheader-enabled page-loading">
+	<!-- Google Tag Manager (noscript) -->
+	<noscript>
+		<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+	</noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<!--begin::Main-->
+	<div class="d-flex flex-column flex-root">
+		<!--begin::Login-->
+		<div class="login login-4 login-signin-on d-flex flex-row-fluid" id="kt_login">
+			<div class="d-flex flex-center flex-row-fluid bgi-size-cover bgi-position-top bgi-no-repeat" style="background-image: url('assets_login/media/bg/bg-11.jpg');">
+				<div class="login-form text-center p-7 position-relative overflow-hidden">
+					<!--begin::Login Header-->
 
-    <!-- PAGE CONTAINER -->
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-    <div id="root" class="root front-container">
-
-        <!-- CONTENTS -->
-        <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-        <section id="content" class="content">
-            <div class="content__boxed w-100 min-vh-100 d-flex flex-column align-items-center justify-content-center">
-                <div class="content__wrap">
-
-                    <!-- Login card -->
-                    <div class="card shadow-lg">
-                        <div class="card-body">
-
-                            <div class="text-center">
-                                <img class="img-lg rounded-circle mb-3" src="assets/img/profile-photos/logo.png" alt="Picture profile">
-                                <h1 class="h3">Admin</h1>
-                                <p>Web Profile
-                                    <br> UOBK RSUD Syarifah Ambami Rato Ebu bang
-                                </p>
-                            </div>
-
-                            <form class="mt-4" action="#"method="POST">
-
-                                <div class="mb-3 <?php if (isset($error)) {
-																	if ($error[0] == "email") echo "alert-validate";
-																} ?>" data-validate="<?php if (isset($error)) {
-																							if ($error[0] == "email") {
-																								echo $error[1];
-																							} else {
-																								echo "Silahkan isi Email.";
-																							}
-																						} else {
-																							echo "Silahkan isi Email.";
-																						} ?>" id="pesanEmail">
-                                    <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="<?= set_value("email"); ?>">
-                                </div>
-
-                                <div class="mb-3 <?php if (isset($error)) {
-																	if ($error[0] == "pass") echo "alert-validate";
-																} ?>" data-validate="<?php if (isset($error)) {
-																							if ($error[0] == "pass") {
-																								echo $error[1];
-																							} else {
-																								echo "Silahkan isi password.";
-																							}
-																						} else {
-																							echo "Silahkan isi password.";
-																						} ?>" id="pesanPass">
-                                    <input type="password" class="form-control"id="pass" name="pass" placeholder="Password">
-                                </div>
-
-                                <!-- <div class="form-check">
-                                    <input id="_dm-loginCheck" class="form-check-input" type="checkbox">
-                                    <label for="_dm-loginCheck" class="form-check-label">
-                                        Remember me
-                                    </label>
-                                </div> -->
-
-                                <div class="d-grid mt-5">
-                                    <button value="Log in" class="btn btn-info btn-lg" id="tmbl_login">
-                                        Login
-                                    </button>
-                                    <!-- <button class="btn btn-info btn-lg" type="submit">Login</button> -->
-                                </div>
-                            </form>
-
-                            <!-- <div class="text-center mt-4">
-                                <a href="#" class="btn-link text-decoration-none">Sign in using different account</a>
-                            </div> -->
-
-                        </div>
-                    </div>
-                    <!-- END : Login card -->
-
-
-                </div>
-            </div>
-        </section>
-
-        <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-        <!-- END - CONTENTS -->
-    </div>
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-    <!-- END - PAGE CONTAINER -->
-
-    <!-- JAVASCRIPTS -->
-    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-
-    <!-- Bootstrap JS [ OPTIONAL ] -->
-    <script src="assets/js/bootstrap.min.bdf649e4bf3fa0261445f7c2ed3517c3f300c9bb44cb991c504bdc130a6ead19.js" defer></script>
-
-    <!-- Nifty JS [ OPTIONAL ] -->
-    <script src="assets/js/nifty.min.736fe03287ab6abf8e0a4e5bd06329bb8e8d21535170616163ea9c3bd96b3cd6.js" defer></script>
+					<div class="d-flex flex-center mb-15">
+						<a href="#">
+							<img src="assets_login/logo.png" class="max-h-75px" alt="" />
+						</a>
+					</div>
+					<!--end::Login Header-->
+					<!--begin::Login Sign in form-->
+					<div class="login-signin">
+						<form class="form" action='#' method="POST">
+							<div class="form-group mb-5 <?php if (isset($error)) {
+															if ($error[0] == "email") echo "alert-validate";
+														} ?>" data-validate="<?php if (isset($error)) {
+																					if ($error[0] == "email") {
+																						echo $error[1];
+																					} else {
+																						echo "Silahkan isi Email.";
+																					}
+																				} else {
+																					echo "Silahkan isi Email.";
+																				} ?>" id="pesanEmail">
+								<input class="form-control h-auto form-control-solid py-4 px-8" type="text" placeholder="Email" name="email" id="email" autocomplete="off" />
+							</div>
+							<div class="form-group mb-5 <?php if (isset($error)) {
+															if ($error[0] == "pass") echo "alert-validate";
+														} ?>" data-validate="<?php if (isset($error)) {
+																					if ($error[0] == "pass") {
+																						echo $error[1];
+																					} else {
+																						echo "Silahkan isi password.";
+																					}
+																				} else {
+																					echo "Silahkan isi password.";
+																				} ?>" id="pesanPass">
+								<input class="form-control h-auto form-control-solid py-4 px-8" type="password" placeholder="Password" id="pass" name="pass" />
+							</div>
+							<button id="tombolLogin" class="btn btn-success font-weight-bold px-9 py-4 my-3 mx-4">Login</button>
+							<br> <a href="<?php echo site_url('Profile'); ?>" class="btn btn-primary">Back Home</a>
+						</form>
+					</div>
+					<!--end::Login Sign in form-->
+				</div>
+			</div>
+		</div>
+		<!--end::Login-->
+	</div>
+	<!--end::Main-->
+	<script>
+		var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";
+	</script>
+	<!--begin::Global Config(global config for global JS scripts)-->
+	<script>
+		var KTAppSettings = {
+			"breakpoints": {
+				"sm": 576,
+				"md": 768,
+				"lg": 992,
+				"xl": 1200,
+				"xxl": 1200
+			},
+			"colors": {
+				"theme": {
+					"base": {
+						"white": "#ffffff",
+						"primary": "#6993FF",
+						"secondary": "#E5EAEE",
+						"success": "#1BC5BD",
+						"info": "#8950FC",
+						"warning": "#FFA800",
+						"danger": "#F64E60",
+						"light": "#F3F6F9",
+						"dark": "#212121"
+					},
+					"light": {
+						"white": "#ffffff",
+						"primary": "#E1E9FF",
+						"secondary": "#ECF0F3",
+						"success": "#C9F7F5",
+						"info": "#EEE5FF",
+						"warning": "#FFF4DE",
+						"danger": "#FFE2E5",
+						"light": "#F3F6F9",
+						"dark": "#D6D6E0"
+					},
+					"inverse": {
+						"white": "#ffffff",
+						"primary": "#ffffff",
+						"secondary": "#212121",
+						"success": "#ffffff",
+						"info": "#ffffff",
+						"warning": "#ffffff",
+						"danger": "#ffffff",
+						"light": "#464E5F",
+						"dark": "#ffffff"
+					}
+				},
+				"gray": {
+					"gray-100": "#F3F6F9",
+					"gray-200": "#ECF0F3",
+					"gray-300": "#E5EAEE",
+					"gray-400": "#D6D6E0",
+					"gray-500": "#B5B5C3",
+					"gray-600": "#80808F",
+					"gray-700": "#464E5F",
+					"gray-800": "#1B283F",
+					"gray-900": "#212121"
+				}
+			},
+			"font-family": "Poppins"
+		};
+	</script>
+	<!--end::Global Config-->
+	<!--begin::Global Theme Bundle(used by all pages)-->
+	<script src="assets_login/plugins/global/plugins.bundle5883.js?v=7.2.9"></script>
+	<script src="assets_login/plugins/custom/prismjs/prismjs.bundle5883.js?v=7.2.9"></script>
+	<script src="assets_login/js/scripts.bundle5883.js?v=7.2.9"></script>
+	<!--end::Global Theme Bundle-->
+	<!--begin::Page Scripts(used by this page)-->
+	<script src="assets_login/js/pages/custom/login/login-general5883.js?v=7.2.9"></script>
+	<!--end::Page Scripts-->
 </body>
+<!--end::Body-->
 
+<!-- Mirrored from preview.keenthemes.com/metronic/demo2/custom/pages/login/classic/login-4.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 13 Jan 2022 06:40:33 GMT -->
 
-<!-- Mirrored from themeon.net/nifty/v3.0.0/front-pages/lock-screen/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 21 Dec 2021 05:26:45 GMT -->
 </html>
